58a9c3762c10539273528f1a9ed7f4bf6b295453
Mahmoud-Elhoseny <elhoseny916@gmail.com>
2024-11-25 00:15:28 +0200

    fix:

--- index.js
+++ index.js
@@ -11,12 +11,14 @@ dotenv.config();
 const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+const port = process.env.PORT || 8000;
+
 app.use(cors({ origin: '*' }));
 app.use(express.json());
 app.use('/auth', authRoutes);
 app.use('/travel', travelRoutes);
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
-app.listen(8000, () => {
-  console.log('Server is running on port 8000');
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
