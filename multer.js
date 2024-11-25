b160cbc10e922a4d3a36b3b90fa05c71604e3d98
Mahmoud-Elhoseny <elhoseny916@gmail.com>
2024-11-25 09:49:15 +0200

    fix:

--- multer.js
+++ multer.js
@@ -11,17 +11,8 @@ const storage = multer.diskStorage({
   },
 });
 
-const fileFilter = (req, file, cb) => {
-  if (file.mimetype.startsWith('image/')) {
-    cb(null, true);
-  } else {
-    cb(new Error('Only images are allowed'), false);
-  }
-};
-
 const upload = multer({
   storage,
-  fileFilter,
 });
 
 export default upload;
